f0d7ed06fcb1f60672c416a8bbd9eb28b48c7950
Agastsya Joshi <agastsya007@gmail.com>
2024-12-22 22:18:09 +0530

    changes before pull

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-green-600 text-2xl">
+      Tailwind CSS is working!
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JSX/TSX files in `src`
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
