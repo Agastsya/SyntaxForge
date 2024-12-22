fd52190d017a336b7c7f4ff65537d1f1160e1b17
Agastsya Joshi <agastsya007@gmail.com>
2024-12-23 01:27:00 +0530

    header created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Homepage/Home";
+import "./App.css";
+import Header from "./Pages/Homepage/Header";
 
 export default function App() {
   return (
-    <Router>
+    <Router className="dark">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}", // Include all JSX/TSX files in `src`
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "serif"], // Add your custom font here
+      },
+      colors: {
+        background: "rgba(var(--background))",
+      },
+    },
   },
   plugins: [],
 };
