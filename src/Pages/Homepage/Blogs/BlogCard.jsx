7f463d9061fd3005d442b2413c818a033b853e9d
agastsya <agastsya007@gmail.com>
2025-06-11 16:44:20 +0530

    blog toggle added

--- src/Pages/Homepage/Blogs/BlogCard.jsx
+++ src/Pages/Homepage/Blogs/BlogCard.jsx
@@ -1,7 +1,27 @@
-export default function BlogCard() {
+export default function BlogCard({
+  author,
+  title,
+  image,
+  date,
+  description,
+  content,
+}) {
   return (
-    <>
-      <div>hello world</div>
-    </>
+    <div className="m-10 bg-gray-800 rounded-md p-5">
+      {/*Top Layer*/}
+      <div className="flex justify-between p-2">
+        <div className="text-white">{author}</div>
+        <div>
+          <div className="text-white">Uploaded : {date.slice(0, 10)}</div>
+        </div>
+      </div>
+      <div className="text-cyan-500 p-2 text-2xl">{title}</div>
+
+      {/*Content Layer Layer*/}
+      <div className="flex p-2 gap-2">
+        <img src={image} className="max-h-[10rem] max-w-[10rem]" alt="" />
+        <div className="text-white">{content}</div>
+      </div>
+    </div>
   );
 }
--- src/Pages/Homepage/Blogs/ReadBlogs.jsx
+++ src/Pages/Homepage/Blogs/ReadBlogs.jsx
@@ -1,12 +1,17 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Blogs from "./Blogs";
+import BlogCard from "./BlogCard";
+import "./ReadBlogs.css";
+const apikey = process.env.NEWS_API_KEY;
 
 export default function ReadBlogs() {
   const [articles, setArticles] = useState([]);
+  const [category, setCategory] = useState("technology");
   useEffect(() => {
     axios
       .get(
-        "https://newsapi.org/v2/top-headlines?country=us&apiKey=9d07c3606dc3415fa7a87b6c50da395d&category=technology"
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apikey}&category=${category}`
       )
       .then((response) => {
         setArticles(response.data.articles);
@@ -14,13 +19,46 @@ export default function ReadBlogs() {
       .catch((error) => {
         console.error(error);
       });
-  }, []);
+  }, [category]);
   return (
     <>
+      <div className="flex gap-10 p-10">
+        <button
+          className="bg-white toggle-category"
+          onClick={() => setCategory("sports")}
+        >
+          business
+        </button>
+        <button
+          className="bg-white toggle-category"
+          onClick={() => setCategory("science")}
+        >
+          science
+        </button>
+        <button
+          className="bg-white toggle-category"
+          onClick={() => setCategory("entertainment")}
+        >
+          entertainment
+        </button>
+        <button
+          className="bg-white toggle-category"
+          onClick={() => setCategory("technology")}
+        >
+          technology
+        </button>
+      </div>
       <div>
         {articles.map((article) => (
           <div key={article.url}>
-            <h1 className="text-white">{article.author}</h1>;
+            <BlogCard
+              author={article.author}
+              title={article.title}
+              image={article.urlToImage}
+              description={article.description}
+              date={article.publishedAt}
+              content={article.content}
+            />
           </div>
         ))}
         ;
